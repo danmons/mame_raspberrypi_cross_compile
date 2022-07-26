7412598338e724d94fc7ce18f6e981aabd659ae6
Dan Mons <dan@mons.com.au>
2022-07-26 14:59:06 +1000

    download nvram and cfg if required

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 usage() {
   echo
   echo "${0} /path/to/gameslist.txt"
@@ -29,6 +28,9 @@ source "${TDIR}/config.ini"
 # Build binary with all path options
 export MBIN="${MAMEDIR}/mame -homepath ${MAMEDIR} -rompath ${MAMEDIR}/roms;${MAMEDIR}/chd -cfg_directory ${MAMEDIR}/cfg -nvram_directory ${MAMEDIR}/nvram"
 
+# Set the download URL for the NVRAM files
+export MURL="https://raw.githubusercontent.com/danmons/mame_raspberrypi_cross_compile/main/benchmark/files"
+
 # Create MAME dirs if missing
 mkdir -p ${MAMEDIR}/roms ${MAMEDIR}/chd ${MAMEDIR}/cfg ${MAMEDIR}/nvram 2>/dev/null
 
@@ -44,6 +46,19 @@ do
   if [ -z "${HASFPS}" ]
   then
     echo Benchmarking "${MROM}"
+    unset NEEDSNVRAM
+    NEEDSNVRAM=$( grep ^"${MROM}"$ "${TDIR}/lists/nvram.txt" )
+    if [ -n "${NEEDSNVRAM}" ]
+    then
+      echo "${MROM} requires NVRAM files to benchmark accurately, downloading them..."
+      cd "${MAMEDIR}/nvram"
+      aria2c --allow-overwrite=true "${MURL}/nvram/${MROM}.7z"
+      7za x -aoa "${MROM}.7z"
+      cd -
+      cd "${MAMEDIR}/cfg"
+      aria2c --allow-overwrite=true "${MURL}/cfg/${MROM}.cfg"
+      cd -
+    fi
     ${MBIN} -bench 90 "${MROM}" >>"${TDIR}/log/${MROM}.log" 2>&1
     sleep 3
   else
